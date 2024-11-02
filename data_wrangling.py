5ba474a1bf028843ecd1538e696d01d3502c77b1
charliewarburton <charliewarburton@hotmail.co.uk>
2024-11-02 16:31:49 +0000

    Pre commit hook clean up update

--- data_wrangling.py
+++ data_wrangling.py
@@ -15,19 +15,26 @@ df = pd.DataFrame(
 )
 
 # Function for if highest earner per household is female
+
+
 def highest_earner_female(df):
+
     def helper(group):
         highest_earner = group.loc[group['income'].idxmax()]
         return highest_earner["female"] == 1
-    
+
     return df.groupby("household_id").apply(helper)
+
 
 female_df = pd.DataFrame(highest_earner_female(df))
 
-#print(female_df.head)
+# print(female_df.head)
 
 # function for the aggregate functions
+
+
 def aggregate_funcs(df):
+
     aggregated_df = df.groupby("household_id").agg(
         {
             "household_id": "count",
@@ -38,22 +45,24 @@ def aggregate_funcs(df):
     )
 
     # aggregated df will have two levels of columns, so we need to flatten it
-    aggregated_df.columns = ['_'.join(col).strip() for col in aggregated_df.columns.values]
+    aggregated_df.columns = ['_'.join(col).strip()
+                             for col in aggregated_df.columns.values]
 
     # rename the columns
     aggregated_df = aggregated_df.rename(
         columns={
-        "household_id_count": "size_hh",
-        "age_mean": "mean_age",
-        "age_min": "min_age",
-        "age_max": "max_age",
-        "income_mean": "mean_income",
-        "income_sum": "total_income",
-        "female_sum": "nr_female"
+            "household_id_count": "size_hh",
+            "age_mean": "mean_age",
+            "age_min": "min_age",
+            "age_max": "max_age",
+            "income_mean": "mean_income",
+            "income_sum": "total_income",
+            "female_sum": "nr_female"
         }
     )
-    
+
     return aggregated_df
+
 
 agg_df = aggregate_funcs(df)
 female_df.columns = ["main_earner_female"]
